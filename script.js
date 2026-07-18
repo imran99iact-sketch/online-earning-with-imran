f4fd4ceae837798575e4c724a047a98fed72c7a7
imran99iact-sketch <imran.99iact@gmail.com>
2026-07-18 19:51:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector("button");
 
+  button.addEventListener("click", function () {
+    alert("Welcome to Online Earning With Imran!");
+  });
+});
